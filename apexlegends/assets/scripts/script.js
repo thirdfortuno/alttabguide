7fc0fe0e2e0100c01d4795ad3563df9243a42f78
Archani <thirdfort@gmail.com>
2019-03-25 00:57:36 +0800

    yay animations work now

--- apexlegends/assets/scripts/script.js
+++ apexlegends/assets/scripts/script.js
@@ -14,10 +14,18 @@ $(document).ready(function() {
 
 });
 
+function tipLoadHelp(tipHTML){
+    $("#box").load(tipHTML);
+}
+
 function getTips(legend){
     legend = legend.toLowerCase();
     tipHTML = "./tips/" + legend + ".html";
-    $('#guide').load(tipHTML);
+    $('#box').fadeOut(0);
+    $('#guide').slideUp(0);
+    tipLoadHelp(tipHTML);
+    $('#box').fadeIn(250);
+    $('#guide').slideDown(250);
     //alert(tipHTML);
 }
 
